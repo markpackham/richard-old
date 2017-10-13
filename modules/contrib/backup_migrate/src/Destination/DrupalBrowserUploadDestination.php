c61feb3b82661332d841484cfedf112cd312a387
Mark Packham <markpackham1@gmail.com>
2017-10-13 06:53:20 +0100

    Added module backup_migrate-8.x-4.0-beta3

--- modules/contrib/backup_migrate/src/Destination/DrupalBrowserUploadDestination.php
+++ modules/contrib/backup_migrate/src/Destination/DrupalBrowserUploadDestination.php
@@ -17,7 +17,7 @@ class DrupalBrowserUploadDestination extends PluginBase implements ReadableDesti
    * {@inheritdoc}
    */
   public function getFile($id) {
-    $file_upload = \Drupal::request()->files->get("files[$id]", NULL, TRUE);
+    $file_upload = \Drupal::request()->files->get("files", NULL, TRUE)[$id];
     // Make sure there's an upload to process.
     if (!empty($file_upload)) {
       $out = new ReadableStreamBackupFile($file_upload->getRealPath());
